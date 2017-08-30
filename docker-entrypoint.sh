cd1917898abb3a3e0ed44e73f52679ab69ef9980
Dong Zhu <bluezhudong@gmail.com>
2017-08-30 11:07:31 +0800

    initial commit: Add the project source code
    Signed-off-by: Dong Zhu <bluezhudong@gmail.com>

---
+++ docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+if [[ -z $ADMIN_PASSWORD ]]; then
+	ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1)
+	echo "########## GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
+fi
+echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}" >> /tmp/glassfishpwd
+domainName=domain1
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name $domainName
+asadmin start-domain
+echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+asadmin --user=admin stop-domain
+
+cp /projectLib/jar/* $GLASSFISH_HOME/glassfish/domains/$domainName/autodeploy/bundles/ 
+cp /projectLib/dbDriver/* $GLASSFISH_HOME/glassfish/domains/$domainName/lib/
+cp $GLASSFISH_HOME/glassfish/domains/$domainName/config/domain.xml /mnt/domain-bak.xml
+cp /projectLib/domain.xml $GLASSFISH_HOME/glassfish/domains/$domainName/config/domain.xml
+
+exec "$@"
+
+logFile=$GLASSFISH_HOME/glassfish/domains/$domainName/logs/server.log
+export logFile
+./start-domain.sh
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# build glassfish
+# docker build --build-arg http_proxy=http://cn-proxy.jp.oracle.com:80 -t glassfish-4.1.2-jdk-8-postgres .
+
+PASSD="oracle"
+
+echo -e "# starting postgres db #"
+#sudo docker run --name postgres -e POSTGRES_PASSWORD=oracle -p 5432:5432 -d postgres:latest
+sudo docker run --name postgres -e POSTGRES_PASSWORD=oracle -d postgres:latest
+
+sleep 30
+
+echo -e "\n# starting glassfish #"
+#sudo docker run -p 8080:8080 -p 4848:4848 -p 8181:8181 --link mysql:mysqlDB glassfish-5.0-db
+#sudo docker run --cap-add=NET_ADMIN -p 8080:8080 -p 4848:4848 -p 8181:8181 -ti -v /home/jude/Docker/glassfish-5.0-db/lib/:/mnt --link postgres:postgres -ti glassfish-4.1.2-jdk-8-postgres /bin/bash
+
+sudo docker run  -p 8080:8080 -p 4848:4848 -p 8181:8181 -ti --link postgres:postgresDBAddr glassfish-4.1.2-jdk-8-postgres
