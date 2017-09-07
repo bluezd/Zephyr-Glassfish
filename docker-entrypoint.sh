340308aeb3d84f14447e74ef1721d7257600fa9e
Dong Zhu <bluezhudong@gmail.com>
2017-09-07 14:39:14 +0800

    Add App Version
    Signed-off-by: Dong Zhu <bluezhudong@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 # Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+
+VER="1.0"
+
+echo "* Starting App Version ($VER) *"
+
 if [[ -z $ADMIN_PASSWORD ]]; then
 	ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1)
 	echo "########## GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
